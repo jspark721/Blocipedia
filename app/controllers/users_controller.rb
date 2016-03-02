14c00e9e496c1d3b27b1dc711356877dc6bc6823
Julie Park <jspark721@gmail.com>
2016-03-02 13:51:15 -0600

    Added params to user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
   def create
-    @user = User.new(params[:user])
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password]
 
-    respond_to do |format|
-      if @user.save
-        UserMailer.welcome_email(@user).deliver_later
-
-        format.html { redirect_to(@user, notice: 'User was successfully created.') }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was an error creating your account. Please try again."
+      render :new
     end
   end
 end
