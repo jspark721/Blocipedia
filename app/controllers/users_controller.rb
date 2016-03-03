3fb2ace8947ec25259212ff4f56eaa0dafc150bc
Julie Park <jspark721@gmail.com>
2016-03-03 12:51:19 -0600

    Created routes for index, edit, update and show for User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
   def new
     @user = User.new
   end
@@ -18,4 +25,20 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  resources :users, only: [:index, :show, :edit, :update, :create]
 
-root 'pages#index'
+  root 'pages#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
