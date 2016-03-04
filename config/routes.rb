43eaa4d652241ce620fbbd90abea681c0648bb34
Julie Park <jspark721@gmail.com>
2016-03-03 19:47:50 -0600

    Generated session controller and rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only:[:edit, :show, :index]
+  resources :users, only: [:edit, :show, :index]
+
+  resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
