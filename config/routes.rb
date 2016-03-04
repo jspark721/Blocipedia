94f056fe2891ad919aa60011b0711b7dca38beeb
Julie Park <jspark721@gmail.com>
2016-03-03 19:56:28 -0600

    Cancel session controller
    This reverts commit 43eaa4d652241ce620fbbd90abea681c0648bb34.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:edit, :show, :index]
-
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only:[:edit, :show, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
