2910114da515c9439aa9725c4a26cc714aefbb89
Julie Park <jspark721@gmail.com>
2016-03-04 02:44:22 -0600

    Added name attribute to database table for User in migration

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20160304060449) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "name",                   default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -28,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160304060449) do
     t.datetime "updated_at",                          null: false
   end
 
+  add_index "users", ["name"], name: "index_users_on_name"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
