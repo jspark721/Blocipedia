da7e836159cfd51a7feb8cb5880310475ca5b883
Julie Park <jspark721@gmail.com>
2016-03-04 11:12:01 -0600

    Added user spec

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20160304060449) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                   default: "", null: false
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) { @user = User.new(name: "example user", email: 'user@example.com', password: "helloworld", password_confirmation: "helloworld") }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:name) }
+
+  it "#email returns a string" do
+    expect(@user.email).to match 'user@example.com'
+  end
 end
