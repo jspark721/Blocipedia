599255f1d3a638042bf8dfad082e217c7e88c24e
Julie Park <jspark721@gmail.com>
2016-03-12 14:37:04 -0600

    Updated user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -21,6 +25,11 @@ class UsersController < ApplicationController
           format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @wikis = @user.wikis
   end
 
   private
