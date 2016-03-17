507f3091da44e9e79fb3ef748ddedf1635aab25b
Julie Park <jspark721@gmail.com>
2016-03-17 15:11:33 -0500

    Added stripe integration and figaro application file

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,2 @@
- # Store the environment variables on the Rails.configuration object
- Rails.configuration.stripe = {
-   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-   secret_key: ENV['STRIPE_SECRET_KEY']
- }
-
- # Set our app-stored secret key with Stripe
- Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = ENV["stripe_api_key"]
+STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
