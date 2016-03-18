1fa4695b319087f0abb53b27abe511cfea81b694
Julie Park <jspark721@gmail.com>
2016-03-17 19:28:34 -0500

    Added choose plan in registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :choose_plan, only: :new
+
+
   def create
     super do |resource|
       if params[:plan]
@@ -6,10 +9,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
         if resource.plan_id == 2
           resource.save_with_payment
         else
-          resouce.save
+          resource.save
         end
       end
     end
   end
-  
+
+  private
+
+  def choose_plan
+    unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2' )
+      flash[:notice] = "Please select a membership plan to sign up."
+      redirect_to root_path
+    end
+  end
+
 end
