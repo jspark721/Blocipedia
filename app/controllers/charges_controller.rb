5647800e1e3bbf9ca2f2948579b67c4c292920cc
Julie Park <jspark721@gmail.com>
2016-03-20 09:55:20 -0500

    Created charges controller and charges view

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,32 @@
+class ChargesController < ApplicationController
+  def new
+    @stripe_btn_data = {
+      key: "#{ Rails.configuration.stripe[:publishable_key] }",
+      description: "BigMoney Membership - #{current_user.name}",
+      amount: @amount
+    }
+  end
+
+  def create
+    @amount = 15_00
+
+    customer = Stripe::Customer.create(
+      email: current_user.email,
+      card: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: "Premium Account - #{current_user.email}",
+      currency: 'usd'
+    )
+
+    flash[:notice] = "Thank you for upgrading to Premium account, #{current_user.name}."
+    redirect_to user_path(current_user)
+  end
+
+rescue Stripe::CardError => e
+  flash.now[:alert] = e.message
+  redirect_to new_charge_path
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :wikis
-
-  get 'welcome/index'
+  resources :charges, only: [:new, :create]
 
   devise_for :users
 
