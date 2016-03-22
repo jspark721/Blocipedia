3c392fd2fe4f8e65624964ddeda42e31b8bb4a15
Julie Park <jspark721@gmail.com>
2016-03-22 15:49:20 -0500

    Fixed wikis helper

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,6 +1,5 @@
 module WikisHelper
   def user_is_authorized_to_delete_wiki?
-    if current_user == @wiki.user || current_user.admin?
-    end
+    current_user == @wiki.user || current_user.admin?
   end
 end
