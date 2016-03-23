01ec4444ef44c2248eea890ce5b92dfc242dd54c
Julie Park <jspark721@gmail.com>
2016-03-23 12:17:22 -0500

    Edited wiki policy to work with no errors

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -1,4 +1,4 @@
 class Collaborator < ActiveRecord::Base
-  belongs_to :wiki_id
-  has_many :user_ids
+  belongs_to :wikis
+  has_many :users
 end
--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -15,7 +15,7 @@ class WikiPolicy < ApplicationPolicy
       elsif user.role == 'premium'
         all_wikis = scope.all
         all_wikis.each do |wiki|
-          if wiki.public? || wiki.owner == user || wiki.collaborators.include?(user)
+          if !(wiki.private?) || wiki.user == user || wiki.collaborators.include?(user)
             wikis << wiki
           end
         end
@@ -23,7 +23,7 @@ class WikiPolicy < ApplicationPolicy
         all_wikis = scope.all
         wikis = []
         all_wikis.each do |wiki|
-          if wiki.public? || wiki.collaborators.include?(user)
+          if !(wiki.private?) || wiki.collaborators.include?(user)
             wikis << wiki
           end
         end
