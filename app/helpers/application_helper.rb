f75e650001a8f29c43325ac7194251d59903bb51
Julie Park <jspark721@gmail.com>
2016-03-23 02:07:21 -0500

    Added markdown gem and added markdown to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    options = {
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+    }
+    redcarpet = Redcarpet::Markdown.new(renderer, options)
+    (redcarpet.render text).html_safe
+  end
 end
