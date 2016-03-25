10997940894793728acf1c4901465800a0174008
Julie Park <jspark721@gmail.com>
2016-03-25 10:53:38 -0500

    Added wiki policy and collaborations controller

---
+++ app/policies/wiki_policy.rb
@@ -0,0 +1,36 @@
+class WikiPolicy < ApplicationPolicy
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      wikis = []
+      if !user
+        wikis = scope.where(private: false)
+      elsif user.role == 'admin'
+        wikis = scope.all
+      elsif user.role == 'premium'
+        all_wikis = scope.all
+        all_wikis.each do |wiki|
+          if !(wiki.private?) || wiki.user == user || wiki.collaborations.include?(user)
+            wikis << wiki
+          end
+        end
+      else
+        all_wikis = scope.all
+        wikis = []
+        all_wikis.each do |wiki|
+          if !(wiki.private?) || wiki.collaborations.include?(user)
+            wikis << wiki
+          end
+        end
+      end
+      wikis
+    end
+  end
+end
